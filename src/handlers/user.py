54111f58172bb358651cf4d80c324ccc690e4bcc
Nalbert Gabriel Melo Leal <nalbertgml@gmail.com>
2021-09-26 17:05:25 -0300

    Added support to create user

--- src/handlers/user.py
+++ src/handlers/user.py
@@ -1,4 +1,12 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+
+from ..models import User
+
+class UserBody(BaseModel):
+  fullname: str
+  email: str
+  password: str
 
 user_router = APIRouter(
   prefix="/user",
@@ -10,3 +18,16 @@ user_router = APIRouter(
 @user_router.get("/")
 async def user_get():
   return { 'name': 'Nalbert' }
+
+@user_router.post("/")
+async def user_create(userBody: UserBody):
+  try:
+    await User.find_by_email(email=userBody.email)
+    return { 'error': 'User already exists' }
+  except:
+    new_user = await User.create(
+      fullname=userBody.fullname,
+      email=userBody.email,
+      password=userBody.password
+    )
+    return new_user
--- src/models/user.py
+++ src/models/user.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Column, String, Integer, DateTime
 from sqlalchemy.sql import func
+from sqlalchemy.future import select
 
-from ..config import Base
+from ..config import Base, async_database_session
 from .base_model import BaseModel
 
 class User(Base, BaseModel):
@@ -9,10 +10,19 @@ class User(Base, BaseModel):
   __mapper_args__ = { "eager_defaults": True }
 
   id = Column(Integer, primary_key=True, autoincrement=True)
-  fullname = Column(String)
-  password = Column(String)
+  fullname = Column(String, nullable=False)
+  email = Column(String, nullable=False)
+  password = Column(String, nullable=False)
   created_at = Column(DateTime(timezone=True), server_default=func.now())
   updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+  @classmethod
+  async def find_by_email(cls, email: str):
+    query = select(cls).where(cls.email == email)
+    results = await async_database_session.execute(query)
+    (result,) = results.one()
+    return result
+
 
   def __repr__(self) -> str:
       return f'{self.id} {self.fullname} {self.password} {self.updated_at}, {self.created_at}'
